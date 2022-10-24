ec5636607da7e3e529af0583be3b12f25c65ac02
knitum <67817925+knitum@users.noreply.github.com>
2022-10-24 17:49:27 +0900

    python_function

---
+++ python_function.py
@@ -0,0 +1,29 @@
+# def name(input):
+#     print('제 이름은 ' + input + '입니다.')
+# name('민준')
+
+# def sum(a,b):
+#     print(a+b)
+# total = sum(3,4)
+# print(total)
+
+# def check_grade(성적,이름):
+#     if 성적 >= 90:
+#         print(이름 + "의 등급은 A입니다.")
+#     elif 80 <= 성적 < 90:
+#         print(이름 + '의 등급은 B입니다.') 
+#     elif 70 <= 성적 < 80:
+#         print(이름 + '의 등급은 C입니다.') 
+#     elif 60 <= 성적 < 70:
+#         print(이름 + '의 등급은 D입니다.') 
+#     else:
+#         print(이름 + '의 등급은 F입니다.') 
+# check_grade(90, '민준')
+
+# a = 'python'
+# a = a.upper()
+# print(a)
+
+import random
+b = random.random()
+print(b)
