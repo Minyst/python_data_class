58794ed1a9aafcf2ed1fe219858b6bfd1910a115
knitum <67817925+knitum@users.noreply.github.com>
2022-10-24 15:32:32 +0900

    파이썬 자료형 데이터
    (1) 숫자형
    (2) 문자형
    (3) 리스트형
    (4) 딕셔너리

---
+++ python_data_type.py
@@ -0,0 +1,48 @@
+x = 'python' 
+x = 123
+x = '123'
+x = [1,2,3,4]
+x = {'name': 'leo', 'age' : 32}
+# print(x['age'])
+x = True
+
+# print(type(x))
+
+a = 1+1
+a = 1001 -1
+a = 12 *12 
+a = 3/4
+
+a = 'Act as though \nit is impossible to fail'
+# print(a)
+
+a = '''
+Act as though 
+it is impossible to fail
+'''
+# print(a)
+c = '10000'
+# print(len(c))
+
+c = 'hello python!'
+c[0]
+# print(c[6:])
+# print(c[:])
+
+x = 'TitanicJames'
+title = x[:7]
+director = x[7:]
+# print(title, director)
+
+a = ['a','b','c','d','e']
+a = ['a','b',['c','d','e']]
+# print(a[2][1])
+
+t = ('a','a','a','b')
+# print(t)
+
+x = {'key1':'value1','key2':'value2'}
+print(x['key2'])
+
+x = set(["1","1","1","2"])
+print(x)
